bbd5af547ed4bba9b6e1a14dc394825d631db12f
karnies <kari@ics.uci.edu>
2014-01-17 11:03:56 -0800

    Updates to get ESMs working on an wakeup alarm.

--- Survey_v1.2.3/src/com/aware/plugin/survey/Alarm.java
+++ Survey_v1.2.3/src/com/aware/plugin/survey/Alarm.java
@@ -1,34 +1,22 @@
 package com.aware.plugin.survey;
 
-
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Vibrator;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.widget.Toast;
 
-import java.util.Calendar;
-
-import com.aware.Aware;
-import com.aware.Aware_Preferences;
 import com.aware.ESM;
+import com.aware.plugin.survey.ESMstorage;
+
 
 public class Alarm extends WakefulBroadcastReceiver {
 
+    final public static String ONE_TIME = "onetime";
 
-	private Plugin plugin;
-	private ESMstorage esms;
-
-	public Alarm(Plugin plugin)
-	{
-		this.plugin = plugin;
-		this.plugin.esms = esms;
-	}
-
+	public Alarm(){}
 
 	private AlarmManager alarmMgr;
 	private PendingIntent alarmIntent;
@@ -36,60 +24,55 @@ public class Alarm extends WakefulBroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {   
 
-
 		Toast t = Toast.makeText(context, "ALARM!!", Toast.LENGTH_LONG);
 		t.show();
 
-		//vibrates phone to let user know of incoming ESMs, let me know if you 
-		//don't want this feature
-		Vibrator vibrator = (Vibrator) context.getSystemService(context.VIBRATOR_SERVICE);
-		vibrator.vibrate(500);
-
-
-		//Make sure ESMs are active within the framework
-		Aware.setSetting(plugin.getContentResolver(), Aware_Preferences.STATUS_ESM, true);
-
-
-		//Queue the ESM to be displayed when possible
-		Intent esm = new Intent(ESM.ACTION_AWARE_QUEUE_ESM);
-
-		esm.putExtra(ESM.EXTRA_ESM, esms.ALL_ESM);
-		context.sendBroadcast(esm);
+		doSurvey(context);
 	}
 
 	public void setAlarm(Context context) {
-		
-		Toast t = Toast.makeText(context, "alarm set started", Toast.LENGTH_LONG);
-		t.show();
-		
+
 		alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+	
 		Intent intent = new Intent(context, Alarm.class);
+		intent.putExtra(ONE_TIME, Boolean.FALSE);
 		alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
-
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTimeInMillis(System.currentTimeMillis());
 		
-		//set the alarm's time to custom...
-		calendar.set(Calendar.HOUR_OF_DAY, 16);
-		calendar.set(Calendar.MINUTE, 35);
-
 		//set alarm to repeat
-		alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP,  
-				calendar.getTimeInMillis(), 
-				1000*15,//AlarmManager.INTERVAL_DAY, 
+	    alarmMgr.setInexactRepeating
+		//alarmMgr.setRepeating
+		       (AlarmManager.RTC_WAKEUP,  
+				System.currentTimeMillis(),
+				AlarmManager.INTERVAL_HOUR, 
+				//1000*60*3, // 3 minutes
 				alarmIntent);
 		
-		Toast u = Toast.makeText(context, "alarm set finished", Toast.LENGTH_LONG);
-		u.show();      
+		Toast t = Toast.makeText(context, "alarm set", Toast.LENGTH_LONG);
+		t.show();
 	}
 
 
 	public void cancelAlarm(Context context) {
 		
-		
 		if (alarmMgr!= null) {
 			alarmMgr.cancel(alarmIntent);
+			Toast u = Toast.makeText(context, "alarm cancelled", Toast.LENGTH_LONG);
+			u.show();
 		}
-
+		
+	}
+	
+	public void doSurvey (Context context) {	
+		Vibrator vibrator = (Vibrator) context.getSystemService(context.VIBRATOR_SERVICE);
+		vibrator.vibrate(500);
+		
+		//Define the ESM to be displayed
+		ESMstorage esms = new ESMstorage();
+		
+		//Queue the ESM to be displayed when possible
+		Intent esm = new Intent(ESM.ACTION_AWARE_QUEUE_ESM);
+		esm.putExtra(ESM.EXTRA_ESM, esms.ALL_ESM);
+		context.sendBroadcast(esm);	
 	}
 }
+
