4a529fc739d7e34c40aa5d8269750b31fb57a40b
Calvin L <Calvin@Calvins-Epic-MacBook-Pro.local>
2014-02-15 18:11:18 -0800

    Notification+ESM working
    Should show up under Survey_v3.1n.

---
+++ Survey_v3.1n/src/com/aware/plugin/survey/Alarm.java
@@ -0,0 +1,159 @@
+package com.aware.plugin.survey;
+
+import java.util.Calendar;
+import java.util.Random;
+
+import android.app.AlarmManager;
+import android.support.v4.app.NotificationCompat;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Vibrator;
+import android.util.Log;
+import android.widget.Toast;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+import com.aware.ESM;
+import com.aware.plugin.survey.Alarm;
+import com.aware.plugin.survey.ESMstorage;
+
+
+public class Alarm extends WakefulBroadcastReceiver {
+
+    final public static String ONE_TIME = "onetime";
+
+	public Alarm(){}
+
+	private AlarmManager alarmMgr;
+	private PendingIntent alarmIntent;
+	private Calendar calendar;
+	
+	private int range;
+	private Random r;
+	
+	MediaPlayer background;
+	AudioManager am;
+
+	public void onReceive(Context context, Intent intent) {   
+
+		Toast t = Toast.makeText(context, "ALARM!!", Toast.LENGTH_LONG);
+		t.show();
+
+		Log.i(Plugin.TAG, "Alarm! received");
+		setAlarm(context);
+		
+		//gets current time
+		calendar.getInstance();
+		
+		//checks if it is within 8am - 3am
+		if((calendar.HOUR_OF_DAY < 3) || (calendar.HOUR_OF_DAY > 8))
+			notifyUser(context);	
+	}
+
+	public void setAlarm(Context context) {
+		Log.i(Plugin.TAG, "Set Alarm");
+		
+		alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+			
+		Intent intent = new Intent(context, Alarm.class);
+		intent.putExtra(ONE_TIME, Boolean.FALSE);
+		alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+		
+		r = new Random();
+		//range = 1000*60*(r.nextInt(76-45) + 45);
+		range = 1000*60*1;
+		
+
+		Toast t = Toast.makeText(context, Integer.toString(range), Toast.LENGTH_LONG);
+		t.show();
+		
+		alarmMgr.set(AlarmManager.RTC_WAKEUP, 
+				(calendar.getInstance().getTimeInMillis() + range), alarmIntent);
+
+		
+		t = Toast.makeText(context, "alarm set", Toast.LENGTH_LONG);
+		t.show();
+		Log.i(Plugin.TAG, "Alarm Set");
+	}
+
+
+	public void cancelAlarm(Context context) {
+		
+		if (alarmMgr!= null) {
+			alarmMgr.cancel(alarmIntent);
+			Toast u = Toast.makeText(context, "alarm cancelled", Toast.LENGTH_LONG);
+			u.show();
+			Log.i(Plugin.TAG, "Alarm Cancelled");
+		}
+		
+	}
+	
+	public void doSurvey (Context context) {	
+
+		//Define the ESM to be displayed
+		ESMstorage esms = new ESMstorage();
+		
+		//Queue the ESM to be displayed when possible
+		Intent esm = new Intent(ESM.ACTION_AWARE_QUEUE_ESM);
+		esm.putExtra(ESM.EXTRA_ESM, esms.ALL_ESM);
+		
+		context.sendBroadcast(esm);	
+	}
+	
+	public void notifyUser(Context context)
+	{
+		Toast t = Toast.makeText(context, "Notify User", Toast.LENGTH_LONG);
+		t.show();
+		Log.i(Plugin.TAG, "Notify User");
+		
+		// prepare intent which is triggered if the
+		// notification is selected
+		
+		int requestID = (int) System.currentTimeMillis();
+		
+		//--trying this part out--
+		//Define the ESM to be displayed
+		ESMstorage esms = new ESMstorage();
+		//create pending ESM intent  
+		Intent esmIntent = new Intent(ESM.ACTION_AWARE_QUEUE_ESM);
+	    esmIntent.putExtra(ESM.EXTRA_ESM, esms.ALL_ESM);
+		
+		//Intent esmIntent = new Intent(context, ESMstorage.class);
+		//esmIntent.addFlags(Intent.FLAG_DEBUG_LOG_RESOLUTION);
+
+	    PendingIntent pIntent = PendingIntent.getBroadcast(context, requestID, esmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+	 
+		// build notification
+		NotificationCompat.Builder mBuilder =
+			    new NotificationCompat.Builder(context)
+        			.setSmallIcon(R.drawable.icon_notification)
+        			.setContentTitle("SURVEY")
+        			.setContentText("Click to start survey.")
+        			.setAutoCancel(true);
+		
+        mBuilder.setContentIntent(pIntent);
+		
+		// Sets an ID for the notification
+		int mNotificationId = (int) System.currentTimeMillis();;
+		// Gets an instance of the NotificationManager service
+		NotificationManager mNotifyMgr = 
+		        (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+		// Builds the notification and issues it.
+		mNotifyMgr.notify(mNotificationId, mBuilder.build());
+		
+		Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+		am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+		
+		if(am.getRingerMode() == AudioManager.RINGER_MODE_NORMAL)
+		{
+			background = MediaPlayer.create(context, R.raw.success);
+			background.setLooping(false);
+			background.start();
+		}
+		vibrator.vibrate(500);
+	}
+}
+
---
+++ Survey_v3.1n/src/com/aware/plugin/survey/ESMstorage.java
@@ -0,0 +1,104 @@
+package com.aware.plugin.survey;
+
+import com.aware.ESM;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class ESMstorage extends BroadcastReceiver {
+
+	//listing ESMs for now, could be changed to readable from external file
+	//instead of being hard coded sometime in the future.
+
+	//buffer
+	final String ESM0 = "{'esm':" +
+			"{'esm_type': 5," +
+			"'esm_title': 'Survey'," +
+			"'esm_instructions': 'Press Next to start...'," +
+			"'esm_quick_answers': ['Next']," +
+			"'esm_expiration_threashold': 2699," +
+			"'esm_trigger': 'AWARE Tester'}}";
+	
+	final String ESM1 = "{'esm':" +
+			"{'esm_type': 2," +
+			"'esm_title':'1/6'," +
+			"'esm_instructions':'What are you doing right now?'," +
+			"'esm_radios':['In class','Studying','Face-to-Face interaction','None of the above']," +
+			"'esm_submit':'Next'," +
+			"'esm_expiration_threashold': 2699," +
+			"'esm_trigger':'AWARE Tester'}}";
+
+	final String ESM2 = "{'esm':" +
+			"{'esm_type': 2," +
+			"'esm_title':'2/6'," +
+			"'esm_instructions':'What is your stress level right now?'," +
+			"'esm_radios':['1 (lowest)','2','3','4','5','6','7 (highest)']," +
+			"'esm_submit':'Next'," +
+			"'esm_expiration_threashold': 2699," +
+			"'esm_trigger':'AWARE Tester'}}";
+
+	final String ESM3 = "{'esm':" +
+			"{'esm_type': 2," +
+			"'esm_title':'3/6'," +
+			"'esm_instructions':'What is your mood right now?'," +
+			"'esm_radios':['1 (negative)','2','3','4','5','6','7 (positive)']," +
+			"'esm_submit':'Next'," +
+			"'esm_expiration_threashold': 2699," +
+			"'esm_trigger':'AWARE Tester'}}";
+
+	final String ESM4 = "{'esm':" +
+			"{'esm_type': 4," +
+			"'esm_title': '4/6'," +
+			"'esm_instructions': 'How focused were you in doing what you were just doing?'," +
+			"'esm_likert_max': 5," +
+			"'esm_likert_max_label': 'extremely'," +
+			"'esm_likert_min_label': 'not at all'," +
+			"'esm_likert_step': 1," +
+			"'esm_submit': 'OK'," +
+			"'esm_expiration_threashold': 2699," +
+			"'esm_trigger': 'AWARE Tester'}}";
+
+	final String ESM5 = "{'esm':" +
+			"{'esm_type': 4," +
+			"'esm_title': '5/6'," +
+			"'esm_instructions': 'How challenging was what you were just doing?'," +
+			"'esm_likert_max': 5," +
+			"'esm_likert_max_label': 'extremely'," +
+			"'esm_likert_min_label': 'not at all'," +
+			"'esm_likert_step': 1," +
+			"'esm_submit': 'OK'," +
+			"'esm_expiration_threashold': 2699," +
+			"'esm_trigger': 'AWARE Tester'}}";
+
+	final String ESM6 = "{'esm':" +
+			"{'esm_type': 4," +
+			"'esm_title': '6/6'," +
+			"'esm_instructions': 'How skilled are you at doing what you were just doing?'," +
+			"'esm_likert_max': 5," +
+			"'esm_likert_max_label': 'extremely'," +
+			"'esm_likert_min_label': 'not at all'," +
+			"'esm_likert_step': 1," +
+			"'esm_submit': 'OK'," +
+			"'esm_expiration_threashold': 2699," +
+			"'esm_trigger': 'AWARE Tester'}}";
+
+	//combines all the esms to be displayed.
+	final String ALL_ESM = "[" + ESM0 + "," + ESM1 + "," + ESM2 + "," + ESM3 + 
+			"," + ESM4 + "," + ESM5 + "," + ESM6 + "]";
+	
+	Toast t;
+
+	public void onReceive (Context context, Intent intent) {
+		
+		t = Toast.makeText(context, "RECEIVED", Toast.LENGTH_LONG);
+		t.show();
+		
+		//Queue the ESM to be displayed when possible
+		Intent esm = new Intent(ESM.ACTION_AWARE_QUEUE_ESM);
+		
+		esm.putExtra(ESM.EXTRA_ESM, ALL_ESM);
+		context.sendBroadcast(esm);	
+	}
+}
